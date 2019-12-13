9764c03ffa338883e502e72f8af96d5e2ecf027c
suzuki84g <>
2019-12-13 15:05:38 +0900

    rest_frameworkの読み込み

--- ikatodo/settings.py
+++ ikatodo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'users.apps.UsersConfig',
     'todolist.apps.TodolistConfig',
     'django.contrib.admin',
