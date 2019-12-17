5d9f2311d7649483791ea133b4562b1b4f1624b6
suzuki84g <>
2019-12-18 00:21:28 +0900

    forms.pyを削除

--- users/admin.py
+++ users/admin.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .forms import UserChangeForm, UserCreationForm
 from .models import User
 
 
 class UserAdmin(BaseUserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
     list_display = ('email', 'username', 'is_admin')
     list_filter = ('is_admin',)
     fieldsets = (
