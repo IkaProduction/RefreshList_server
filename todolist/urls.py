4dd20fbb1ed78321c78ecb044760a23cd411b5a5
suzuki84g <>
2019-12-19 07:20:49 +0900

    初期参照先をアプリapiに変更

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,10 +4,12 @@ from . import views
 from rest_framework import routers
 from .virewset import TodoViewSet, UserViewSet
 
+# todo: Defaultのviewの連結なので削除
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
 ]
 
-router = routers.DefaultRouter()
-router.register('todos', TodoViewSet)
-router.register('users', UserViewSet)
+# todo: DRF関連の不要な設定
+# router = routers.DefaultRouter()
+# router.register('todos', TodoViewSet)
+# router.register('users', UserViewSet)
