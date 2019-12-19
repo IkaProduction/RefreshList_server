d455813b8153816af84fb45cb93b2f629d602e3d
suzuki84g <>
2019-12-19 10:10:03 +0900

    review箇所の修正

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from .views import TodoViewSet, LabelViewSet
 from rest_framework import routers
 
-urlpatterns = [
-]
+urlpatterns = []
 
 router = routers.SimpleRouter()
 router.register('todos', TodoViewSet)
 router.register('labels', LabelViewSet)
 
-# todo: user関連のSerializer設定
+# todo: user関連のrouter設定を一旦オミット。ユーザー登録、ログイン、ログアウトの実装に併せて修正か削除します。
 # router.register('users', UserViewSet)
--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from todolist.models import Todo, Label
 from .serializers import TodoSerializer, LabelSerializer
 from rest_framework import viewsets
-from django.contrib.auth import get_user_model
 
 
 class TodoViewSet(viewsets.ModelViewSet):
@@ -14,7 +13,8 @@ class LabelViewSet(viewsets.ModelViewSet):
     serializer_class = LabelSerializer
 
 
-# # todo: user関連のSerializer設定
+# todo: user関連のSerializer設定を一旦オミット。ユーザー登録、ログイン、ログアウトの実装に併せて修正か削除します。
+# from django.contrib.auth import get_user_model
 # class UserViewSet(viewsets.ModelViewSet):
 #     queryset = get_user_model().objects.all()
 #     serializer_class = UserSerializer
