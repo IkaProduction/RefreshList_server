f9909108dee2e69322044384658675425c91113a
suzuki84g <>
2019-12-20 16:50:43 +0900

    適切な読み書き属性を付与

--- api/serializers.py
+++ api/serializers.py
@@ -7,15 +7,19 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'user_id', 'title', 'finished_flag', 'deadline', 'important', 'memo')
+        read_only_fields = ('id', 'user_id')
 
 
 class LabelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Label
-        fields = ('user_id', 'title', 'coler_code')
+        fields = ('title', 'coler_code')
+        read_only_fields = ('User_id')
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username')
+        fields = ('id', 'email', 'username', 'password')
+        write_only_fields = ('password')
+        read_only_fields = ('id',)
