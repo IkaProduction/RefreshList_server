40753708d382a0cd3c40b76cde3a0f8c57d234da
suzuki84g <>
2019-12-21 21:34:44 +0900

    ユーザー認証（ログイン）処理を実装

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include
 from django.urls import path
-from .views import TodoViewSet, LabelViewSet, UserViewSet, UserCreateView
+from .views import TodoViewSet, LabelViewSet, UserViewSet, UserCreateView, CheckView, SessionView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -12,4 +12,6 @@ router.register('users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('sign_up/', UserCreateView.as_view()),
+    path('login-check/', CheckView.as_view()),  # FIXME: ログインチェック用
+    path('login-session/', SessionView.as_view()),  # FIXME: ユーザー認証をしてセッションIDを発行
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
 from todolist.models import Todo, Label
+from django.contrib.auth import get_user_model
 from .serializers import TodoSerializer, LabelSerializer, UserSerializer
 from rest_framework import viewsets, generics
-from django.contrib.auth import get_user_model
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate, login
 
 
 class TodoViewSet(viewsets.ModelViewSet):
@@ -22,3 +26,24 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 class UserCreateView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+
+class CheckView(APIView):  # NOTE:セッション確認
+    authentication_classes = (SessionAuthentication, )
+
+    def get(self, request, format=None):
+        content = {'user': str(request.user), 'auth': str(request.auth)}
+        return Response(content)
+
+
+class SessionView(APIView):  # NOTE:ユーザー認証が完了するとセッションIDが返答される
+
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({'session': request.session.session_key})
+
+# TODO: ログアウト処理を書く
