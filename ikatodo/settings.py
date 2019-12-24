16201b5a20827e28efb158729b4acc663b877e03
suzuki84g <>
2019-12-25 00:58:55 +0900

    サードパーティのパッケージを追加及びコメントの追記

--- ikatodo/settings.py
+++ ikatodo/settings.py
@@ -31,10 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # NOTE:startapp
     'api.apps.ApiConfig',
     'users.apps.UsersConfig',
     'todolist.apps.TodolistConfig',
+    # NOTE:3rd party packages
     'rest_framework',
+    'django_filters',
+    # NOTE:default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -53,8 +57,8 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',  # NOTE: 標準で全てのCRUDが認証必須
-    ]
+        'rest_framework.permissions.IsAuthenticated',  # NOTE:標準で全てのCRUDが認証必須
+    ],
 }
 
 MIDDLEWARE = [
