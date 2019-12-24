bc309249f0bf67df0a8fa6e542a6921674b4dbf1
suzuki84g <>
2019-12-25 01:07:14 +0900

    コメントの表記ブレを修正

--- api/urls.py
+++ api/urls.py
@@ -11,8 +11,8 @@ router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('sign-up/', UserCreateView.as_view()),  # NOTE: ユーザー作成
-    path('login/', LoginView.as_view()),  # NOTE: ユーザー認証をしてセッションIDを発行
-    path('logout/', LogoutView.as_view()),  # NOTE: ログアウト
-    path('user-check/', CheckView.as_view()),  # NOTE: セッションとユーザー確認用
+    path('sign-up/', UserCreateView.as_view()),  # NOTE:ユーザー作成
+    path('login/', LoginView.as_view()),  # NOTE:ユーザー認証をしてセッションIDを発行
+    path('logout/', LogoutView.as_view()),  # NOTE:ログアウト
+    path('user-check/', CheckView.as_view()),  # NOTE:セッションとユーザー確認用
 ]
